28c2151b8322fdaa9f42dbf210570de3aee97db4
J3rey <jeremiayo@gmail.com>
2025-09-05 23:55:12 +1000

    refactor: simplify MongoDB connection options

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.once('ready', async () => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(console.error);
 
